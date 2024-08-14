4e39a7fbf590f2c715a215a5748be316c167b4ac
Egor <lotus9200@gmail.com>
2024-08-14 13:44:36 +0600

    chore(main): added dependencies

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from contextlib import asynccontextmanager
 from data.config import API
 
 from database import init_database, close_database
-from routers import user_router, position_router
+from services import UserService, PositionService
+
+from container import container
 
 import logging
-
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
@@ -22,9 +23,19 @@ async def lifespan(app: FastAPI):
     logger.info('[👋] Bye')
     await close_database()
 
+# Registration dependencies
+user_service = UserService()
+position_service = PositionService()
+
+container.attach("user_service", user_service)
+container.attach("position_service", position_service)
+
 app = FastAPI(title='USER REST API', debug=True, lifespan=lifespan)
+
+from routers import user_router, position_router
 app.include_router(user_router, prefix='/user', tags=['user'])
 app.include_router(position_router, prefix='/position', tags=['position'])
+
 
 if __name__ == '__main__':
     run(app, host=API.host, port=API.port)
