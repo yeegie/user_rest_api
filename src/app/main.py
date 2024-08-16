c0823fb0693f38bab19ee5b77497a5531db9de66
Egor <lotus9200@gmail.com>
2024-08-16 13:58:44 +0600

    chore: dependency update

--- src/app/main.py
+++ src/app/main.py
@@ -4,14 +4,25 @@ from contextlib import asynccontextmanager
 
 from data.app_config import API
 
+from repositories import BaseRoleRepository, BaseUserRepository
 from services import DatabaseService
+
 from data.database_config import DataBase
 
+from container import container
+
+# Logging
 import logging
+from logging import Logger
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
+# Repositories
+user_repository = BaseUserRepository()
+role_repository = BaseRoleRepository()
 
+# Services
 database_service = DatabaseService(
     type=DataBase.type,
     host=DataBase.host,
@@ -21,6 +32,10 @@ database_service = DatabaseService(
     database=DataBase.database,
 )
 
+container.attach(DatabaseService, database_service)
+container.attach(BaseUserRepository, user_repository)
+container.attach(BaseRoleRepository, role_repository)
+container.attach(Logger, logger)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -28,14 +43,15 @@ async def lifespan(app: FastAPI):
     await database_service.init_database()
 
     yield
+    
     logger.info('[👋] Bye')
     await database_service.close_database()
 
 app = FastAPI(title='USER REST API', debug=True, lifespan=lifespan)
 
-from routers import user_router, position_router
+from routers import user_router, role_router
 app.include_router(user_router, prefix='/user', tags=['user'])
-app.include_router(position_router, prefix='/position', tags=['position'])
+app.include_router(role_router, prefix='/role', tags=['role'])
 
 
 if __name__ == '__main__':
