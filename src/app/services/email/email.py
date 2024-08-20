43b7f09f6fb7e00d4ccc51bd554d712f3db27a7e
Egor <lotus9200@gmail.com>
2024-08-20 14:43:17 +0600

    feat: move EmailServer to root dir

--- src/app/services/email/email.py
+++
@@ -1,9 +0,0 @@
-from .base import BaseEmailService
-
-
-class EmailService(BaseEmailService):
-    def send_welcome_message(self, recipient: str) -> None:
-        print("Hello, this is welcome message.")
-
-    def send_code(sef, recipient: str, code: str) -> None:
-        print(f"Hello, your code: {code}")
