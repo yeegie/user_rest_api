05b1b1cd6bf0d29900c1ab11a48359d51be956aa
Egor <lotus9200@gmail.com>
2024-08-22 10:26:26 +0600

    chore: refactoring service

--- src/app/services/role.py
+++ src/app/services/role.py
@@ -1,27 +1,29 @@
 from schemas.role import RoleSchema, RoleCreateDto, RoleUpdateDto
+from repositories import BaseRoleRepository
+from logging import Logger
 
 
 class RoleService():
-    def __init__(self, role_repository, logger) -> None:
-        self._role_repository = role_repository
-        self._logger = logger
+    def __init__(self, role_repository: BaseRoleRepository, logger: Logger) -> None:
+        self.__repository = role_repository
+        self.__logger = logger
 
     async def create(self, dto: RoleCreateDto):
         """### Create position from dto"""
-        return await self._role_repository.create(dto)
+        return await self.__repository.create(dto)
 
     async def read(self, id: int) -> RoleSchema:
         """### Get positon from id"""
-        return await self._role_repository.read(role_id=id)
+        return await self.__repository.read(role_id=id)
 
     async def update(self, id: int, dto: RoleUpdateDto):
         """### Update user from dto by id"""
-        return await self._role_repository.update(role_id=id, dto=dto)
+        return await self.__repository.update(role_id=id, dto=dto)
 
     async def delete(self, id: int):
         """### Delete Role by role_id"""
-        return await self._role_repository.delete(role_id=id)
+        return await self.__repository.delete(role_id=id)
 
     async def all(self):
         """### Get all roles"""
-        return await self._role_repository.all()
+        return await self.__repository.all()
